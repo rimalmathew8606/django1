52ba5445ab5f2fe150e9e9ca9ec8a3a9939db7a4
rimal mathew <rimalmathew8606@gmail.com>
2024-02-07 08:34:13 +0530

    added

--- ecommerce/cart/models.py
+++ ecommerce/cart/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 from shop.models import Product
 from django.contrib.auth.models import User
-
+from django.http import HttpResponse
 
 class Cart(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
